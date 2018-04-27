bf63847b62f398d1b7b8c2c8f4b4e8830e5373bb
QiHaiTao <1205507034@qq.com>
2018-04-27 16:50:39 +0800

    事物开启

--- src/main/java/com/xjyq/XjyqApplication.java
+++ src/main/java/com/xjyq/XjyqApplication.java
@@ -4,8 +4,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 @ComponentScan(basePackages ={"com.xjyq.controller","com.xjyq.service", "com.xjyq.Aspc"})
 @MapperScan(basePackages = {"com.xjyq.mapper"})
 public class XjyqApplication {
