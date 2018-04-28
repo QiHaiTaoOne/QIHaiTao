10b38ccadafb65ac8f7ad6e0f685b0f2c7c61e3e
QiHaiTao <1205507034@qq.com>
2018-04-28 10:40:45 +0800

    Task

--- src/main/java/com/xjyq/controller/common/ThreadExecutor.java
+++ src/main/java/com/xjyq/controller/common/ThreadExecutor.java
@@ -8,7 +8,6 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.ThreadPoolExecutor;
-import java.util.logging.Logger;
 
 /***
  * 线程池配置
