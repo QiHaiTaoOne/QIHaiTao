a6f62f32f1082f75238a744b6deca752415e271a
QiHaiTao <1205507034@qq.com>
2018-04-28 10:29:48 +0800

    线程池配置

---
+++ src/main/java/com/xjyq/controller/common/ThreadExecutor.java
@@ -0,0 +1,42 @@
+package com.xjyq.controller.common;
+
+import lombok.extern.log4j.Log4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.logging.Logger;
+
+/***
+ * 线程池配置
+ */
+
+@Configuration
+@EnableAsync
+public class ThreadExecutor {
+
+    private int corePoolSize = 10;//线程池维护线程的最少数量
+
+    private int maxPoolSize = 30;//线程池维护线程的最大数量
+
+    private int queueCapacity = 8; //缓存队列
+
+    private int keepAlive = 60;//允许的空闲时间
+
+    @Bean
+    public Executor myExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(corePoolSize);
+        executor.setMaxPoolSize(maxPoolSize);
+        executor.setQueueCapacity(queueCapacity);
+        executor.setThreadNamePrefix("mqExecutor-");
+        // CALLER_RUNS：不在新线程中执行任务，而是由调用者所在的线程来执行
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy()); //对拒绝task的处理策略
+        executor.setKeepAliveSeconds(keepAlive);
+        executor.initialize();
+        return executor;
+    }
+}
