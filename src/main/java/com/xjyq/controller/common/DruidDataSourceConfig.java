c4d1832e0bfd503d2b9e9fff719c38e4381cfa29
QiHaiTao <1205507034@qq.com>
2018-04-28 10:08:52 +0800

    多环境配置

--- src/main/java/com/xjyq/controller/common/DruidDataSourceConfig.java
+++ src/main/java/com/xjyq/controller/common/DruidDataSourceConfig.java
@@ -50,6 +50,12 @@ public class DruidDataSourceConfig implements EnvironmentAware {
     @Value("${spring.datasource.minEvictableIdleTimeMillis}")
     private int minEvictableIdleTimeMillis;
 
+    @Value("${spring.datasource.DruidName}")
+    private String DruidName;
+
+    @Value("${spring.datasource.DruidPassword}")
+    private String DruidPassword;
+
 
     @Override
     public void setEnvironment(Environment environment) {
@@ -82,8 +88,8 @@ public class DruidDataSourceConfig implements EnvironmentAware {
         servletRegistrationBean.setServlet(new StatViewServlet());
         servletRegistrationBean.addUrlMappings("/druid/*");
         Map<String, String> initParameters = new HashMap<String, String>();
-        // initParameters.put("loginUsername", "druid");// 用户名
-        // initParameters.put("loginPassword", "druid");// 密码
+         initParameters.put("loginUsername", DruidName);// 用户名
+         initParameters.put("loginPassword", DruidPassword);// 密码
         initParameters.put("resetEnable", "false");// 禁用HTML页面上的“Reset All”功能
         initParameters.put("allow", "127.0.0.1"); // IP白名单 (没有配置或者为空，则允许所有访问)
         // initParameters.put("deny", "192.168.20.38");// IP黑名单
